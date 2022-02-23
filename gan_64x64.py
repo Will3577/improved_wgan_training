254c37f9a8d4962bc6bdd5d83e005253e68bef3f
Hengrui  Wang <u6202156@anu.edu.au>
2022-02-23 18:39:51 +1100

    code prep

--- gan_64x64.py
+++ gan_64x64.py
@@ -482,7 +482,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
 
     for device_index, (device, real_data_conv) in enumerate(zip(DEVICES, split_real_data_conv)):
         with tf.device(device):
-            print(real_data_conv.shape)
+            print(tf.shape(real_data_conv))
             real_data = tf.reshape(2*((tf.cast(real_data_conv, tf.float32)/255.)-.5), [BATCH_SIZE//len(DEVICES), OUTPUT_DIM])
             fake_data = Generator(BATCH_SIZE/len(DEVICES))
 
@@ -580,7 +580,6 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
     else:
         all_fixed_noise_samples = tf.concat(0, all_fixed_noise_samples)
     def generate_image(iteration):
-        print("testing")
         samples = session.run(all_fixed_noise_samples)
         samples = ((samples+1.)*(255.99/2)).astype('int32')
         lib.save_images.save_images(samples.reshape((BATCH_SIZE, 3, 64, 64)), 'samples_{}.png'.format(iteration))
