90b9e9b0e30afe55c8f42c5b339d36b56f69c8bb
Hengrui  Wang <u6202156@anu.edu.au>
2022-02-23 11:54:40 +1100

    code prep

--- tflib/plot.py
+++ tflib/plot.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 import collections
 import time
-# import piclke
+import piclke
 # import cPickle as pickle
 
 _since_beginning = collections.defaultdict(lambda: {})
