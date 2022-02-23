cc27f9a51a1799b48606f40ba6ba9eb8ad4b24a4
Hengrui  Wang <u6202156@anu.edu.au>
2022-02-24 10:41:19 +1100

    code prep

--- gan_64x64.py
+++ gan_64x64.py
@@ -495,7 +495,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
             # print(tf.shape(real_data_conv))
             real_data = tf.reshape(2*((tf.cast(real_data_conv, tf.float32)/255.)-.5), [BATCH_SIZE//len(DEVICES), OUTPUT_DIM])
             fake_data = Generator(BATCH_SIZE//len(DEVICES))
-            print(real_data.get_shape(), fake_data.get_shape(), BATCH_SIZE//len(DEVICES))
+            # print(real_data.get_shape(), fake_data.get_shape(), BATCH_SIZE//len(DEVICES))
             disc_real = Discriminator(real_data)
             disc_fake = Discriminator(fake_data)
 
