99607f64d39df25964566d59917eefc01f544e4d
Hengrui  Wang <u6202156@anu.edu.au>
2022-02-23 15:01:56 +1100

    code prep

--- tflib/inception_score.py
+++ tflib/inception_score.py
@@ -47,6 +47,7 @@ def get_inception_score(images, splits=10):
         # sys.stdout.flush()
         inp = inps[(i * bs):min((i + 1) * bs, len(inps))]
         inp = np.concatenate(inp, 0)
+        print(inp.shape)
         pred = sess.run(softmax, {'ExpandDims:0': inp})
         preds.append(pred)
     preds = np.concatenate(preds, 0)
