f1dcc729532513c6c5bd010d3efc085a5e14ea28
Hengrui  Wang <u6202156@anu.edu.au>
2022-02-24 00:12:00 +1100

    code prep

--- tflib/ops/cond_batchnorm.py
+++ tflib/ops/cond_batchnorm.py
@@ -1,13 +1,23 @@
 import tflib as lib
 
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+try:
+    # Python 2
+    xrange
+except NameError:
+    # Python 3, xrange is now named range
+    xrange = range
+
 
 def Batchnorm(name, axes, inputs, is_training=None, stats_iter=None, update_moving_stats=True, fused=True, labels=None, n_labels=None):
     """conditional batchnorm (dumoulin et al 2016) for BCHW conv filtermaps"""
     if axes != [0,2,3]:
         raise Exception('unsupported')
-    mean, var = tf.nn.moments(inputs, axes, keep_dims=True)
+    mean, var = tf.nn.moments(inputs, axes, keepdims=True)
     shape = mean.get_shape().as_list() # shape is [1,n,1,1]
     offset_m = lib.param(name+'.offset', np.zeros([n_labels,shape[1]], dtype='float32'))
     scale_m = lib.param(name+'.scale', np.ones([n_labels,shape[1]], dtype='float32'))
